ef5c8aea3a31ce47b91e9540aff86260172e30a5
SeyithanDilek <iletisim.seyithandilek@gmail.com>
2021-02-27 00:27:56 +0300

    Completed Register Packages

---
+++ src/main/java/com/seyithandilek/demo/registration/RegistrationController.java
@@ -0,0 +1,24 @@
+package com.seyithandilek.demo.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    @PostMapping
+    public String reqister(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
+
+
+
+
+}
---
+++ src/main/java/com/seyithandilek/demo/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,16 @@
+package com.seyithandilek.demo.registration.token;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken token){
+        confirmationTokenRepository.save(token);
+    }
+
+}
